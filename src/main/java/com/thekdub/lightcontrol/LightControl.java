7a4136cfec6d2287698b610ac23132add0854a94
TheKDub <12961363+thekdub@users.noreply.github.com>
2022-04-04 15:21:21 -0400

    Added message when no valid serial ports are detected. Added messages stating which port is being used for which universe. Swapped a few variables to final.

--- src/main/java/com/thekdub/lightcontrol/LightControl.java
+++ src/main/java/com/thekdub/lightcontrol/LightControl.java
@@ -13,21 +13,28 @@ public class LightControl {
       System.out.println("Usage: java -jar lightcontrol.jar <port1> <port2> ...\n");
       System.out.println("\tEach port provided will be assigned to each universe in order.");
       System.out.println("Valid Ports:");
-      for (String port : DMXCommunicator.getValidSerialPorts()) {
-        System.out.printf("\t> %s\n", port);
+      if (DMXCommunicator.getValidSerialPorts().size() > 0) {
+        for (String port : DMXCommunicator.getValidSerialPorts()) {
+          System.out.printf("\t> %s\n", port);
+        }
+      }
+      else {
+        System.out.println("\tNo valid serial ports detected.");
       }
       return;
     }
 
     final List<DMXCommunicator> PORTS = new ArrayList<>();
 
-    for (String port : args) {
+    for (int i = 0; i < args.length; i++) {
+      final String PORT = args[i];
       try {
-        PORTS.add(new DMXCommunicator(port));
+        PORTS.add(new DMXCommunicator(PORT));
+        System.out.printf("\tUsing port %s for universe %d\n", PORT, i+1);
       }
       catch (Exception e) {
         System.out.printf("Unable to connect to port %s! Double check your spelling and make sure the " +
-              "adapter is connected!\n", port);
+              "adapter is connected!\n", PORT);
         return;
       }
     }
@@ -39,17 +46,17 @@ public class LightControl {
 
     System.out.println("System ready! Enter <address: 1-" + PORTS.size() * 512 + "> <value: 0-255> to set a value.");
     System.out.println("Type 'q' to exit.");
-    Scanner scanner = new Scanner(System.in);
+    final Scanner SCANNER = new Scanner(System.in);
     while (true) {
       System.out.print("> ");
-      String input = scanner.nextLine();
-      if (input.matches("[0-9]+ [0-9]+")) {
+      final String INPUT = SCANNER.nextLine();
+      if (INPUT.matches("[0-9]+ [0-9]+")) {
         try {
-          int address = Math.min(PORTS.size() * 512-1, Math.max(0, Integer.parseInt(input.split(" ")[0])-1));
-          byte value = (byte) Math.min(255, Math.max(0, Integer.parseInt(input.split(" ")[1])));
-          int universe = address / 512;
-          address = address - universe * 512;
-          PORTS.get(universe).setByte(address, (byte) value);
+          int address = Math.min(PORTS.size() * 512-1, Math.max(0, Integer.parseInt(INPUT.split(" ")[0])-1));
+          final byte VALUE = (byte) Math.min(255, Math.max(0, Integer.parseInt(INPUT.split(" ")[1])));
+          final int UNIVERSE = address / 512;
+          address = address - UNIVERSE * 512;
+          PORTS.get(UNIVERSE).setByte(address, (byte) VALUE);
         }
         catch (Exception e) {
           System.out.println("Invalid input! Please use format <address: 1-" + PORTS.size() * 512 + "> <value: 0-255>");
@@ -57,7 +64,7 @@ public class LightControl {
         }
       }
       else {
-        if (input.equalsIgnoreCase("q") || input.equalsIgnoreCase("quit")) {
+        if (INPUT.equalsIgnoreCase("q") || INPUT.equalsIgnoreCase("quit")) {
           break;
         }
         else {
